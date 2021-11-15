0349ead204aa47798b2cd6d2f1a7180ce18e3074
only41l <vladik41lbra@gmail.com>
2021-11-15 17:49:20 +0200

    2_task

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,9 +1,25 @@
-var x, y;
+/*var x, y;
 x = prompt("Enter x:", x);
-if (Math.log(x) > Math.pow(x, 2)) 
+if (Math.log(x) + Math.pow(x, 2) < 0) {
     y = Math.cos(Math.pow(x, 2) + Math.log(x));
-else if (Math.log(x) == Math.pow(x, 2)) 
+    debugger
+}
+else if (Math.log(x) + Math.pow(x, 2) == 0) {
     y = 1/(Math.pow(x, 2) + Math.log(x));
-else 
+    debugger
+}
+else {
     y = Math.cos(x);
-document.write(y.toFixed(2));
+    debugger
+}
+debugger
+alert(y.toFixed(2));*/
+var num = 0; 
+var summ = 0;
+while(num != null){
+    num = prompt("Enter number", num);
+    if (num > 9 && num < 100) summ += Number(num);
+    debugger
+}
+debugger
+alert("Сума двоцифрових чисел: " + summ);
